8ea14262bc07c2562e9707e15771777bdfd67268
LuisDF04 <luisediazdf@gmail.com>
2025-02-14 09:08:03 +0000

    geraEsfera (falta testar)

--- src/gerador/gerador.cpp
+++ src/gerador/gerador.cpp
@@ -4,6 +4,9 @@
 #include <cstring>
 #include "figuras/Triangles.h"
 #include "ficheiro3d/Ficheiro.h"
+#include <math.h>
+
+#define PI 3.14159265358979323846
 
 using namespace std;
 
@@ -11,6 +14,41 @@ vector<Triangle> geraEsfera(double radius, double slides, double stacks) {
 
     //TODO:
     vector<Triangle> lista;
+
+    for (int i = 0; i < stacks; ++i) {
+        float phi1 = PI * i / stacks;
+        float phi2 = PI * (i + 1) / stacks;
+
+        for (int j = 0; j < slides; ++j) {
+            float theta1 = 2.0f * PI * j / slides;
+            float theta2 = 2.0f * PI * (j + 1) / slides;
+
+            float x1 = radius * sin(phi1) * cos(theta1);
+            float y1 = radius * cos(phi1);
+            float z1 = radius * sin(phi1) * sin(theta1);
+
+            float x2 = radius * sin(phi2) * cos(theta1);
+            float y2 = radius * cos(phi2);
+            float z2 = radius * sin(phi2) * sin(theta1);
+
+            float x3 = radius * sin(phi1) * cos(theta2);
+            float y3 = radius * cos(phi1);
+            float z3 = radius * sin(phi1) * sin(theta2);
+
+            float x4 = radius * sin(phi2) * cos(theta2);
+            float y4 = radius * cos(phi2);
+            float z4 = radius * sin(phi2) * sin(theta2);
+
+            Ponto p1 = Ponto(x1,y1,z1);
+            Ponto p2 = Ponto(x2,y2,z2);
+            Ponto p3 = Ponto(x3,y3,z3);
+            Ponto p4 = Ponto(x4,y4,z4);
+
+            lista.push_back(Triangle(p1,p2,p3));
+            lista.push_back(Triangle(p2,p4,p3));
+        }
+    }
+
     return lista;
 
 }
