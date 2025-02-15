1bacd48b47c9db400972433b14b62b694199ac0a
afbc-04 <andrefbc2004@gmail.com>
2025-02-15 22:25:11 +0000

    Alterado engine adicionando variaveis

--- src/engine/engine.cpp
+++ src/engine/engine.cpp
@@ -12,11 +12,42 @@
 #include <vector>
 #include "figuras/Triangles.h"
 #include "ficheiro3d/Ficheiro.h"
+#include "tinyxml/tinyxml.h"
 
 using namespace std;
 
-vector<Triangle> lista;
+//Variaveis
+vector<vector<Triangle>> modelos_a_carregar; //Lista com os modelos a carregar
 
+//Janela
+int height = 0;
+int width = 0;
+
+//Posição
+double pos_x = 0;
+double pos_y = 0;
+double pos_z = 0;
+
+//Vetor de visão
+double camera_x = 0;
+double camera_y = 0;
+double camera_z = 0;
+
+//Vetor Normal
+double normal_x = 0;
+double normal_y = 0;
+double normal_z = 0;
+
+//Projeção
+double fov = 0;
+double near = 0;
+double far = 0;
+
+//Modo debug
+bool modo_debug = false;
+int quantidade_max_triangulos = 0;
+
+// -------
 
 void changeSize(int w, int h)
 {
@@ -31,7 +62,7 @@ void changeSize(int w, int h)
 	// Load the identity matrix
 	glLoadIdentity();
 	// Set the perspective
-	gluPerspective(45.0f, ratio, 1.0f, 1000.0f);
+	gluPerspective(fov,ratio,near,far);
 	// return to the model view matrix mode
 	glMatrixMode(GL_MODELVIEW);
 
@@ -39,72 +70,105 @@ void changeSize(int w, int h)
 	glViewport(0, 0, w, h);
 }
 
+//Função que desenha
 void renderScene(void) {
+
+	//Iniciando a camera e posição ##########
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    glLoadIdentity();
-    gluLookAt(3.0f, 2.0f, 1.0f,  // Câmera ajustada
-              0.0f, 0.0f, 0.0f,
-              0.0f, 1.0f, 0.0f);
-
-	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
-	gluPerspective(60.0f,1,1,1000);
-	glMatrixMode(GL_MODELVIEW);
+    gluLookAt(pos_x, pos_y, pos_z,  // Câmera ajustada
+              camera_x, camera_y, camera_z,
+              normal_x, normal_y, normal_z);
 
-    glColor3f(0.0f, 1.0f, 0.0f);  // Verde para o plano
-    
-    //CHAT
-    //glDisable(GL_CULL_FACE);
-
-    
-    glPolygonMode(GL_FRONT, GL_LINE);
-    glBegin(GL_TRIANGLES);
-    
-    for (Triangle t : lista) {
-
-        glColor3f(1,1,1);
-
-		Ponto a = t.p3;
-		Ponto b = t.p2;
-		Ponto c = t.p1;
-
-        /*
-        
-        TESTAR POSIÇÃO
-        
-        */
-
-	   glVertex3f(a.x,a.y,a.z);
-	   glVertex3f(b.x,b.y,b.z);
-	   glVertex3f(c.x,c.y,c.z);
-
-    }
-    
-
-
-    glEnd();
-
-    glPushMatrix();
-glRotatef(90, 1, 0, 0);  // Rotação de 90 graus no eixo X
-//glutWireSphere(1, 10, 10);
-glPopMatrix();
-
-    // Eixos para referência
+	//Eixos ##########
     glBegin(GL_LINES);
-    glColor3f(1.0f, 0.0f, 0.0f); // Eixo X vermelho
+
+	//Eixo X
+    glColor3f(1.0f, 0.0f, 0.0f);
     glVertex3f(-100.0f, 0.0f, 0.0f);
     glVertex3f(100.0f, 0.0f, 0.0f);
 
-    glColor3f(0.0f, 1.0f, 0.0f); // Eixo Y verde
+	//Eixo Y
+    glColor3f(0.0f, 1.0f, 0.0f);
     glVertex3f(0.0f, -100.0f, 0.0f);
     glVertex3f(0.0f, 100.0f, 0.0f);
 
-    glColor3f(0.0f, 0.0f, 1.0f); // Eixo Z azul
+	//Eixo Z
+    glColor3f(0.0f, 0.0f, 1.0f);
     glVertex3f(0.0f, 0.0f, -100.0f);
     glVertex3f(0.0f, 0.0f, 100.0f);
+
+    glEnd();
+
+
+	//Desenhar figuras ##########
+    glPolygonMode(GL_FRONT, GL_LINE);
+    glBegin(GL_TRIANGLES);
+    
+	int i = 0;
+	glColor3f(1,1,1);
+
+    for (vector<Triangle> lista : modelos_a_carregar) {
+
+		for (Triangle t : lista) {
+
+			if (modo_debug == false || (modo_debug == true && i <= quantidade_max_triangulos)) {
+			
+				//Obter pontos
+				Ponto a = t.p1;
+				Ponto b = t.p2;
+				Ponto c = t.p3;
+
+				//Desenhar os pontos
+	   			glVertex3f(a.x,a.y,a.z);
+	   			glVertex3f(b.x,b.y,b.z);
+	   			glVertex3f(c.x,c.y,c.z);
+
+			}
+
+			i++;
+
+		}
+
+    }
+    
     glEnd();
 
     glutSwapBuffers();
+}
+
+//Função que processa as teclas especiais
+void pressSpecialKey(int key, int x, int y) {
+	/*
+    switch (key) {
+    case GLUT_KEY_UP:    angleX -= 5.0f; break;
+    case GLUT_KEY_DOWN:  angleX += 5.0f; break;
+    case GLUT_KEY_LEFT:  angleY -= 5.0f; break;
+    case GLUT_KEY_RIGHT: angleY += 5.0f; break;
+    }*/
+	glutPostRedisplay();
+}
+
+//Função que processa as teclas normais
+void pressKey(unsigned char key, int x, int y) {
+    switch (key) {
+    /*case '+': targetZoom -= 0.5f; break;
+    case '-': targetZoom += 0.5f; break;*/
+
+		//Modo debug
+		case 'l':
+			modo_debug = true;
+			quantidade_max_triangulos = 0;
+			break;
+		
+		//Aumentar a quantidade máxima de triangulos
+		case 'k':
+			quantidade_max_triangulos++;
+			break;
+
+    }
+
+	glutPostRedisplay();
 }
 
 void printInfo() {
@@ -116,24 +180,53 @@ void printInfo() {
 
 int main(int argc, char** argv)
 {
-	//Nota : trabalho em modo release
+	//Inicialização
+	height = 512;
+	width = 512;
+
+	//Posição
+	pos_x = 3;
+	pos_y = 2;
+	pos_z = 1;
+
+	//Vetor de visão
+	camera_x = 0;
+	camera_y = 0;
+	camera_z = 0;
+
+	//Vetor Normal
+	normal_x = 0;
+	normal_y = 1;
+	normal_z = 0;
+
+	//Projeção
+	fov = 60;
+	near = 1;
+	far = 1000;
+
 
 	// put GLUT init here
 	glutInit(&argc,argv);
 	glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
     glutInitWindowPosition(100,100);
-	glutInitWindowSize(512,512);
-	glutCreateWindow("Janela");
+	glutInitWindowSize(width,height);
+	glutCreateWindow("Projeto CG - 24/25");
+
+	//Carregar os modelos
+	for (int i = 1 ; i < argc ; i++) {
+		vector<Triangle> novo_modelo_carregado = lerBinario(argv[i]);
+		modelos_a_carregar.push_back(novo_modelo_carregado);
+	}
 
 	// put callback registry here
-    //glutKeyboardFunc(&pressKey);
-
-	lista = lerBinario(argv[1]);
+	glutKeyboardFunc(&pressKey);
+	glutSpecialFunc(&pressSpecialKey);
 
 	glutDisplayFunc(&renderScene);
 	glutReshapeFunc(&changeSize);
-	glutIdleFunc(&renderScene);
-	
+	glutPostRedisplay();
+	//glutIdleFunc(&renderScene);
+
 	// some OpenGL settings
 	glEnable(GL_DEPTH_TEST);
 	glEnable(GL_CULL_FACE);
